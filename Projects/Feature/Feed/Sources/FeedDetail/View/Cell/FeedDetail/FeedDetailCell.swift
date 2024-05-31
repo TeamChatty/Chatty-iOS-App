bf632ff4cb7f075e3bd2d33739e7b2d9b48aebc5
yoonjiho37 <109410688+yoonjiho37@users.noreply.github.com>
2024-05-31 22:11:49 +0900

    fix: rx cell 재사용 시 subscribe 취소 [IOS-53] (#114)
    Co-authored-by: yoonjiho37 <yoonjiho37@github.com>

--- Projects/Feature/Feed/Sources/FeedDetail/View/Cell/FeedDetail/FeedDetailCell.swift
+++ Projects/Feature/Feed/Sources/FeedDetail/View/Cell/FeedDetail/FeedDetailCell.swift
@@ -68,6 +68,11 @@ final class FeedDetailCell: UITableViewCell, Touchable {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    self.disposeBag = DisposeBag()
+  }
 
   // MARK: - UIConfigurable
   private func configureUI() {
